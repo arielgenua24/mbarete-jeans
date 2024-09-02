0dcea079d66d8acb2c21c4035539fd886d430c34
Ariel Genua <arielgenua@MacBook-Air-de-Ariel.local>
2024-09-02 18:08:56 -0300

    installed and launched perfectly vite + react + html

---
+++ vite-project/PAGES/APP/App.jsx
@@ -0,0 +1,16 @@
+// import { useRoutes, BrowserRouter } from 'react-router-dom'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p> hola</p>
+      </div>
+    </>
+  )
+}
+
+export default App
