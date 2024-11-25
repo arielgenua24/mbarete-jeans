08a578224c4d4b01a8a5d0ba960e02a03d50c8fb
ariel <arielgenua24@gmail.com>
2024-11-25 14:38:17 -0300

    added transformations to the price span

--- vite-project/components/addToCartModal/index.jsx
+++ vite-project/components/addToCartModal/index.jsx
@@ -136,7 +136,11 @@ const Modal = () => {
 
           <div className="modal-actions">
             <button className="modal-delete" onClick={onDelete}>Eliminar todo y salir</button>
-            <button className="modal-add" onClick={submit}>Guardar y salir</button>
+            <div  className="div-modal-add">
+              <img  className="div-modal-add-image" src="../../public/images/logos/icons8-download-24.png"></img>
+              <button className="modal-add" onClick={submit}>Guardar y salir</button>
+            </div>
+           
           </div>
         
         </div>
